7ab1dff2a597448746dd804fa9632ac6639dbadf
ViktorSvertoka <victor.svertoka@gmail.com>
2025-04-02 00:27:40 +0300

    feat(js): add decorators to controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,46 +1,40 @@
 import * as contactsService from '../services/contactsServices.js';
 import HttpError from '../helpers/HttpError.js';
+import ctrlWrapper from '../decorators/ctrlWrapper.js';
 
-export const getAllContacts = async (req, res) => {
+export const getAllContacts = ctrlWrapper(async (_, res) => {
   const data = await contactsService.listContacts();
-
   res.json(data);
-};
+});
 
-export const getOneContact = async (req, res) => {
+export const getOneContact = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
   const data = await contactsService.getContactById(id);
   if (!data) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
-
   res.json(data);
-};
+});
 
-export const createContact = async (req, res) => {
+export const createContact = ctrlWrapper(async (req, res) => {
   const data = await contactsService.addContact(req.body);
-
   res.status(201).json(data);
-};
+});
 
-export const updateContact = async (req, res) => {
+export const updateContact = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
   const data = await contactsService.updateContacts(id, req.body);
-
   if (!data) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
-
   res.json(data);
-};
+});
 
-export const deleteContact = async (req, res) => {
+export const deleteContact = ctrlWrapper(async (req, res) => {
   const { id } = req.params;
   const data = await contactsService.removeContact(id);
-
   if (!data) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
-
   res.status(204).send();
-};
+});
